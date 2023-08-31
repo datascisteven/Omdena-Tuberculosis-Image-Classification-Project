57d9050d6c4f427fb9e217a1f43926ee8eeb87e3
Steven Yan <examsherpa@Steven-MacBook.local>
2023-08-31 18:57:06 -0400

    update flask

--- flask/app.py
+++ flask/app.py
@@ -1,12 +1,130 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 17 15:40:29 2018
+
+@author: Kaushik
+"""
+
+from flask import Flask, render_template, request, session, redirect, url_for, flash
 import os
-from flask import Flask
+from werkzeug.utils import secure_filename
+from tensorflow.keras.models import load_model
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
 
-app = Flask(__name__)
-env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
-app.config.from_object(env_config)
+UPLOAD_FOLDER = './flask app/assets/images'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+# Create Database if it doesnt exist
+
+app = Flask(__name__, static_url_path='/assets',
+            static_folder='./flask app/assets', 
+            template_folder='./templates')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-@app.route("/")
+@app.route('/')
+def root():
+   return render_template('index.html')
+
+@app.route('/index.html')
 def index():
-    secret_key = app.config.get("SECRET_KEY")
-    return f"The configured secret key is {secret_key}."
+   return render_template('index.html')
+
+@app.route('/testing.html')
+def contact():
+   return render_template('testing.html')
+
+@app.route('/treatment.html')
+def news():
+   return render_template('treatment.html')
+
+@app.route('/about.html')
+def about():
+   return render_template('about.html')
+
+@app.route('/faqs.html')
+def faqs():
+   return render_template('faqs.html')
+
+@app.route('/prevention.html')
+def prevention():
+   return render_template('prevention.html')
+
+@app.route('/upload.html')
+def upload():
+   return render_template('upload.html')
+
+@app.route('/results.html')
+def results():
+   return render_template('results.html')
+
+
+
+
+@app.route('/uploaded_chest', methods = ['POST', 'GET'])
+def uploaded_chest():
+   if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            # filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'upload_chest.jpg'))
+
+   tb_model = load_model('models/xception_chest.h5')
+
+   image = cv2.imread('./flask app/assets/images/upload_chest.jpg') # read file 
+   image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # arrange format as per keras
+   image = cv2.resize(image,(224,224))
+   image = np.array(image) / 255
+   image = np.expand_dims(image, axis=0)
+   
+   resnet_pred = resnet_chest.predict(image)
+   probability = resnet_pred[0]
+   print("Resnet Predictions:")
+   if probability[0] > 0.5:
+      resnet_chest_pred = str('%.2f' % (probability[0]*100) + '% COVID') 
+   else:
+      resnet_chest_pred = str('%.2f' % ((1-probability[0])*100) + '% NonCOVID')
+   print(resnet_chest_pred)
+
+   vgg_pred = vgg_chest.predict(image)
+   probability = vgg_pred[0]
+   print("VGG Predictions:")
+   if probability[0] > 0.5:
+      vgg_chest_pred = str('%.2f' % (probability[0]*100) + '% COVID') 
+   else:
+      vgg_chest_pred = str('%.2f' % ((1-probability[0])*100) + '% NonCOVID')
+   print(vgg_chest_pred)
+
+   inception_pred = inception_chest.predict(image)
+   probability = inception_pred[0]
+   print("Inception Predictions:")
+   if probability[0] > 0.5:
+      inception_chest_pred = str('%.2f' % (probability[0]*100) + '% COVID') 
+   else:
+      inception_chest_pred = str('%.2f' % ((1-probability[0])*100) + '% NonCOVID')
+   print(inception_chest_pred)
+
+   xception_pred = xception_chest.predict(image)
+   probability = xception_pred[0]
+   print("Xception Predictions:")
+   if probability[0] > 0.5:
+      xception_chest_pred = str('%.2f' % (probability[0]*100) + '% COVID') 
+   else:
+      xception_chest_pred = str('%.2f' % ((1-probability[0])*100) + '% NonCOVID')
+   print(xception_chest_pred)
+
+   return render_template('results_chest.html',tb_pred=tb_pred)
+
+if __name__ == '__main__':
+   app.secret_key = ".."
+   app.run()
---
+++ utils.py
@@ -0,0 +1,373 @@
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+from PIL import ImageFile
+from sklearn.metrics import confusion_matrix
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Flatten, Dropout, BatchNormalization, Conv2D, MaxPool2D, MaxPooling2D
+
+
+def sixplot(var, auc, v_auc, pr_auc, v_pr_auc, prec, v_prec, rec, v_rec):
+    """
+        Function to produce the plots for all the 6  metrics measured during training as specified in compiling the model:
+            accuracy, ROC-AUC, PR-AUC, recall, precision, and loss
+        `var` is the variable that we set `model.fit` or `model.fit_generator`
+        `auc`, `pr_auc`, `rec`, `prec`, etc. must be strings with quotes, i.e. `'auc_4'` or `'precision_8'`
+    """
+
+    f, axs = plt.subplots(2, 3, figsize=(14, 8))
+    axs[0, 0].plot(var.history['accuracy'], label = 'train')
+    axs[0, 0].plot(var.history['val_accuracy'], label = 'validation')
+    axs[0, 0].set_xlabel('Epoch')
+    axs[0, 0].set_ylabel('Accuracy')
+    axs[0, 0].legend()
+    axs[0, 0].set_title('Accuracy Scores')
+    
+    axs[0, 1].plot(var.history[auc], label = 'train')
+    axs[0, 1].plot(var.history[v_auc], label = 'validation')
+    axs[0, 1].set_xlabel('Epoch')
+    axs[0, 1].set_ylabel('ROC-AUC')
+    axs[0, 1].legend()
+    axs[0, 1].set_title('ROC/AUC Scores')
+    
+    axs[0, 2].plot(var.history[pr_auc], label = 'train')
+    axs[0, 2].plot(var.history[v_pr_auc], label = 'validation')
+    axs[0, 2].set_xlabel('Epoch')
+    axs[0, 2].set_ylabel('PR-AUC')
+    axs[0, 2].legend()
+    axs[0, 2].set_title('PR/AUC Scores')
+    
+    axs[1, 0].plot(var.history[prec], label = 'train')
+    axs[1, 0].plot(var.history[v_prec], label = 'validation')
+    axs[1, 0].set_xlabel('Epoch')
+    axs[1, 0].set_ylabel('Precision')
+    axs[1, 0].legend()
+    axs[1, 0].set_title('Precision Scores')
+    
+    axs[1, 1].plot(var.history[rec], label = 'train')
+    axs[1, 1].plot(var.history[v_rec], label = 'validation')
+    axs[1, 1].set_xlabel('Epoch')
+    axs[1, 1].set_ylabel('Recall')
+    axs[1, 1].legend()
+    axs[1, 1].set_title('Recall Scores')
+    
+    axs[1, 2].plot(var.history['loss'], label = 'train')
+    axs[1, 2].plot(var.history['val_loss'], label = 'validation')
+    axs[1, 2].set_xlabel('Epoch')
+    axs[1, 2].set_ylabel('Loss')
+    axs[1, 2].legend()
+    axs[1, 2].set_title('Loss Scores')
+
+    f.tight_layout(h_pad=5, w_pad=5)
+
+def sixplot2(df, auc, v_auc, pr_auc, v_pr_auc, prec, v_prec, rec, v_rec):
+    """
+        Function (after uploading from dataframe from CSVLogger) to produce the plots for the training metrics
+        `df` is the variable that we set `model.fit` or `model.fit_generator`
+        `auc`, `pr_auc`, `rec`, `prec`, etc. must be strings with quotes, i.e. `'auc_4'` or `'precision_8'`
+    """
+
+    f, axs = plt.subplots(2, 3, figsize=(14, 8))
+    axs[0, 0].plot(df['accuracy'], label = 'train')
+    axs[0, 0].plot(df['val_accuracy'], label = 'validation')
+    axs[0, 0].set_xlabel('Epoch')
+    axs[0, 0].set_ylabel('Accuracy')
+    axs[0, 0].legend()
+    axs[0, 0].set_title('Accuracy Scores')
+    
+    axs[0, 1].plot(df[auc], label = 'train')
+    axs[0, 1].plot(df[v_auc], label = 'validation')
+    axs[0, 1].set_xlabel('Epoch')
+    axs[0, 1].set_ylabel('ROC-AUC')
+    axs[0, 1].legend()
+    axs[0, 1].set_title('ROC/AUC Scores')
+    
+    axs[0, 2].plot(df[pr_auc], label = 'train')
+    axs[0, 2].plot(df[v_pr_auc], label = 'validation')
+    axs[0, 2].set_xlabel('Epoch')
+    axs[0, 2].set_ylabel('PR-AUC')
+    axs[0, 2].legend()
+    axs[0, 2].set_title('PR/AUC Scores')
+    
+    axs[1, 0].plot(df[prec], label = 'train')
+    axs[1, 0].plot(df[v_prec], label = 'validation')
+    axs[1, 0].set_xlabel('Epoch')
+    axs[1, 0].set_ylabel('Precision')
+    axs[1, 0].legend()
+    axs[1, 0].set_title('Precision Scores')
+    
+    axs[1, 1].plot(df[rec], label = 'train')
+    axs[1, 1].plot(df[v_rec], label = 'validation')
+    axs[1, 1].set_xlabel('Epoch')
+    axs[1, 1].set_ylabel('Recall')
+    axs[1, 1].legend()
+    axs[1, 1].set_title('Recall Scores')
+    
+    axs[1, 2].plot(df['loss'], label = 'train')
+    axs[1, 2].plot(df['val_loss'], label = 'validation')
+    axs[1, 2].set_xlabel('Epoch')
+    axs[1, 2].set_ylabel('Loss')
+    axs[1, 2].legend()
+    axs[1, 2].set_title('Loss Scores')
+
+    f.tight_layout(h_pad=5, w_pad=5)
+
+
+def make_confusion_matrix(y, y_pred):
+    cnf = confusion_matrix(y, y_pred)
+    group_names = ['TN','FP','FN','TP']
+    group_counts = ['{0:0.0f}'.format(value) for value in cnf.flatten()]
+    group_percentages = ['{0:.2%}'.format(value) for value in cnf.flatten()/np.sum(cnf)]
+    labels = [f'{v1}\n{v2}\n{v3}' for v1, v2, v3 in zip(group_names, group_counts, group_percentages)]
+    labels = np.asarray(labels).reshape(2,2)
+    sns.heatmap(cnf, annot=labels, fmt='', cmap='Blues', annot_kws={'size':16})
+
+def make_confusion_matrix_3(y, y_pred):
+    cnf = confusion_matrix(y, y_pred)
+    group_names = ['TP','FN','FP','TN']
+    group_counts = ['{0:0.0f}'.format(value) for value in cnf.flatten()]
+    group_percentages = ['{0:.2%}'.format(value) for value in cnf.flatten()/np.sum(cnf)]
+    labels = [f'{v1}\n{v2}\n{v3}' for v1, v2, v3 in zip(group_names, group_counts, group_percentages)]
+    labels = np.asarray(labels).reshape(2,2)
+    sns.heatmap(cnf, annot=labels, fmt='', cmap='Blues', annot_kws={'size':16})
+
+def draw_confusion_matrix(cf,
+                          group_names=None,
+                          figsize=None,
+                          title=None):
+
+    group_labels = ["{}\n".format(value) for value in group_names]
+    group_counts = ["{0:0.0f}\n".format(value) for value in cf.flatten()]
+    group_percentages = ["{0:.2%}".format(value) for value in cf.flatten()/np.sum(cf)]
+    box_labels = [f"{v1}{v2}{v3}".strip() for v1, v2, v3 in zip(group_labels,group_counts,group_percentages)]
+    box_labels = np.asarray(box_labels).reshape(cf.shape[0],cf.shape[1])
+
+    accuracy  = np.trace(cf) / float(np.sum(cf))
+    precision = cf[1,1] / sum(cf[:,1])
+    recall    = cf[1,1] / sum(cf[1,:])
+    f1_score  = 2*precision*recall / (precision + recall)
+    stats_text = "\n\nAccuracy={:0.3f}\nPrecision={:0.3f}\nRecall={:0.3f}\nF1 Score={:0.3f}".format(
+                accuracy, precision, recall, f1_score)
+
+    plt.figure(figsize=figsize)
+    sns.heatmap(cf, annot=box_labels, fmt="", cmap='Blues', cbar=False, annot_kws={'size':16})
+    plt.xlabel(stats_text, fontsize=16)
+    plt.title(title, fontsize=16)
+
+
+def train_val_metrics(epochs, model_training):
+    """
+        Function to produce loss and validation graphs after training
+        epochs is the number of epochs specified during model training, model+training is the name of rhis  
+    """    
+
+    epochs = range(1, epochs+1)
+    metrics = model_training.history
+    train_loss = metrics['loss']
+    train_acc = metrics['accuracy']
+    val_loss = metrics['val_loss']
+    val_acc = metrics['val_accuracy']
+    
+    ax = plt.subplot(211)
+    train, = ax.plot(epochs, train_loss)
+    val, = ax.plot(epochs, val_loss)
+    ax.legend([train, val], ['training', 'validation'])
+    ax.set(xlabel='epochs', ylabel='categorical cross-entropy loss')
+
+    ax2 = plt.subplot(212)
+    train2, = ax2.plot(epochs, train_acc)
+    val2, = ax2.plot(epochs, val_acc)
+    ax2.legend([train2, val2], ['training', 'validation'])
+    ax2.set(xlabel='epochs', ylabel='accuracy')
+
+    plt.tight_layout(h_pad=5, w_pad=5)
+
+
+def train_val_metrics2(epochs, metrics):
+    epochs = range(1, epochs+1)
+    train_loss = metrics['loss']
+    train_acc = metrics['accuracy']
+    val_loss = metrics['val_loss']
+    val_acc = metrics['val_accuracy']
+    
+    ax = plt.subplot(211)
+    train, = ax.plot(epochs, train_loss)
+    val, = ax.plot(epochs, val_loss)
+    ax.legend([train, val], ['training', 'validation'])
+    ax.set(xlabel='epochs', ylabel='loss')
+
+    ax2 = plt.subplot(212)
+    train2, = ax2.plot(epochs, train_acc)
+    val2, = ax2.plot(epochs, val_acc)
+    ax2.legend([train2, val2], ['training', 'validation'])
+    ax2.set(xlabel='epochs', ylabel='accuracy')
+
+    plt.tight_layout(h_pad=5, w_pad=5)
+
+
+## courtesy of flaboss on Github
+def stratified_sample(df, strata, size=None, seed=None, keep_index= True):
+    '''
+    It samples data from a pandas dataframe using strata. These functions use
+    proportionate stratification:
+    n1 = (N1/N) * n
+    where:
+        - n1 is the sample size of stratum 1
+        - N1 is the population size of stratum 1
+        - N is the total population size
+        - n is the sampling size
+    Parameters
+    ----------
+    :df: pandas dataframe from which data will be sampled.
+    :strata: list containing columns that will be used in the stratified sampling.
+    :size: sampling size. If not informed, a sampling size will be calculated
+        using Cochran adjusted sampling formula:
+        cochran_n = (Z**2 * p * q) /e**2
+        where:
+            - Z is the z-value. In this case we use 1.96 representing 95%
+            - p is the estimated proportion of the population which has an
+                attribute. In this case we use 0.5
+            - q is 1-p
+            - e is the margin of error
+        This formula is adjusted as follows:
+        adjusted_cochran = cochran_n / 1+((cochran_n -1)/N)
+        where:
+            - cochran_n = result of the previous formula
+            - N is the population size
+    :seed: sampling seed
+    :keep_index: if True, it keeps a column with the original population index indicator
+    
+    Returns
+    -------
+    A sampled pandas dataframe based in a set of strata.
+    Examples
+    --------
+    >> df.head()
+    	id  sex age city 
+    0	123 M   20  XYZ
+    1	456 M   25  XYZ
+    2	789 M   21  YZX
+    3	987 F   40  ZXY
+    4	654 M   45  ZXY
+    ...
+    # This returns a sample stratified by sex and city containing 30% of the size of
+    # the original data
+    >> stratified = stratified_sample(df=df, strata=['sex', 'city'], size=0.3)
+    Requirements
+    ------------
+    - pandas
+    - numpy
+    '''
+    population = len(df)
+    size = __smpl_size(population, size)
+    tmp = df[strata]
+    tmp['size'] = 1
+    tmp_grpd = tmp.groupby(strata).count().reset_index()
+    tmp_grpd['samp_size'] = round(size/population * tmp_grpd['size']).astype(int)
+
+    # controlling variable to create the dataframe or append to it
+    first = True 
+    for i in range(len(tmp_grpd)):
+        # query generator for each iteration
+        qry=''
+        for s in range(len(strata)):
+            stratum = strata[s]
+            value = tmp_grpd.iloc[i][stratum]
+            n = tmp_grpd.iloc[i]['samp_size']
+
+            if type(value) == str:
+                value = "'" + str(value) + "'"
+            
+            if s != len(strata)-1:
+                qry = qry + stratum + ' == ' + str(value) +' & '
+            else:
+                qry = qry + stratum + ' == ' + str(value)
+        
+        # final dataframe
+        if first:
+            stratified_df = df.query(qry).sample(n=n, random_state=seed).reset_index(drop=(not keep_index))
+            first = False
+        else:
+            tmp_df = df.query(qry).sample(n=n, random_state=seed).reset_index(drop=(not keep_index))
+            stratified_df = stratified_df.append(tmp_df, ignore_index=True)
+    
+    return stratified_df
+
+
+def stratified_sample_report(df, strata, size=None):
+    '''
+    Generates a dataframe reporting the counts in each stratum and the counts
+    for the final sampled dataframe.
+    Parameters
+    ----------
+    :df: pandas dataframe from which data will be sampled.
+    :strata: list containing columns that will be used in the stratified sampling.
+    :size: sampling size. If not informed, a sampling size will be calculated
+        using Cochran adjusted sampling formula:
+        cochran_n = (Z**2 * p * q) /e**2
+        where:
+            - Z is the z-value. In this case we use 1.96 representing 95%
+            - p is the estimated proportion of the population which has an
+                attribute. In this case we use 0.5
+            - q is 1-p
+            - e is the margin of error
+        This formula is adjusted as follows:
+        adjusted_cochran = cochran_n / 1+((cochran_n -1)/N)
+        where:
+            - cochran_n = result of the previous formula
+            - N is the population size
+    Returns
+    -------
+    A dataframe reporting the counts in each stratum and the counts
+    for the final sampled dataframe.
+    '''
+    population = len(df)
+    size = __smpl_size(population, size)
+    tmp = df[strata]
+    tmp['size'] = 1
+    tmp_grpd = tmp.groupby(strata).count().reset_index()
+    tmp_grpd['samp_size'] = round(size/population * tmp_grpd['size']).astype(int)
+    return tmp_grpd
+
+def __smpl_size(population, size):
+    '''
+    A function to compute the sample size. If not informed, a sampling 
+    size will be calculated using Cochran adjusted sampling formula:
+        cochran_n = (Z**2 * p * q) /e**2
+        where:
+            - Z is the z-value. In this case we use 1.96 representing 95%
+            - p is the estimated proportion of the population which has an
+                attribute. In this case we use 0.5
+            - q is 1-p
+            - e is the margin of error
+        This formula is adjusted as follows:
+        adjusted_cochran = cochran_n / 1+((cochran_n -1)/N)
+        where:
+            - cochran_n = result of the previous formula
+            - N is the population size
+    Parameters
+    ----------
+        :population: population size
+        :size: sample size (default = None)
+    Returns
+    -------
+    Calculated sample size to be used in the functions:
+        - stratified_sample
+        - stratified_sample_report
+    '''
+    if size is None:
+        cochran_n = round(((1.96)**2 * 0.5 * 0.5)/ 0.02**2)
+        n = round(cochran_n/(1+((cochran_n -1) /population)))
+    elif size >= 0 and size < 1:
+        n = round(population * size)
+    elif size < 0:
+        raise ValueError('Parameter "size" must be an integer or a proportion between 0 and 0.99.')
+    elif size >= 1:
+        n = size
+    return n
+
+
